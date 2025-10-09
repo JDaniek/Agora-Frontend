dd4c83d7e2f7d89ab3c38c6d4b91593b2c2f42dc
JDaniek <danielorantes270@gmail.com>
2025-10-08 18:48:04 -0600

    Nueva actualizacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
-
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/landing-page/landing.page').then(m => m.LandingPage) },];
+  { path: '', loadComponent: () => import('./pages/landing-page/landing.page').then(m => m.LandingPage) },
+
+
+];

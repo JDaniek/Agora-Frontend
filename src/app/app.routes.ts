669fa47ffd3211eb7050fffa44651b8a5431723c
JDaniek <danielorantes270@gmail.com>
2025-10-09 21:07:05 -0600

    Responsive agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/landing-page/landing.page').then(m => m.LandingPage) },
-  { path: 'signup', loadComponent: () => import('./pages/signup/signup').then(m => m.Signup) }
+  { path: 'signup', loadComponent: () => import('./pages/signup/signup').then(m => m.Signup) },
+    { path: 'login', loadComponent: () => import('./pages/login/login').then(m => m.Login) }
 ];
+ 
--- src/app/pages/landing-page/landing.page.ts
+++ src/app/pages/landing-page/landing.page.ts
@@ -3,14 +3,66 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
   templateUrl: './landing.page.html',
   styleUrl: './landing.page.css'
 })
 export class LandingPage {
-    constructor(private router: Router) {}
+  constructor(private router: Router) {}
+
+  // Tus imágenes
+  carouselImages = [
+    '/Card1.png',
+    '/Card2.png',
+    '/Card3.png',
+    '/Card4.png',
+  ];
+
+  currentIndex = 1; // Empezamos con la segunda imagen centrada (índice 1)
+
+  // Mostrar 4 imágenes: 1 izquierda, 1 centro, 2 derecha (ajustable)
+  get visibleSlides(): string[] {
+    const start = Math.max(0, this.currentIndex - 1);
+    const end = Math.min(this.carouselImages.length, start + 4);
+    return this.carouselImages.slice(start, end);
+  }
+
+  isActive(index: number): boolean {
+        const actualIndex = this.carouselImages.indexOf(this.visibleSlides[index]);
+  return actualIndex === this.currentIndex;
+  }
+
+  isLeft(index: number): boolean {
+    const actualIndex = this.carouselImages.indexOf(this.visibleSlides[index]);
+  return actualIndex < this.currentIndex;
+  }
+
+  isRight(index: number): boolean {
+      const actualIndex = this.carouselImages.indexOf(this.visibleSlides[index]);
+  return actualIndex > this.currentIndex;
+  }
+
+  prev(): void {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+scrollToDiferencia(): void {
+  const target = document.getElementById('diferencia');
+  if (target) {
+    target.scrollIntoView();
+  }
+}
+  next(): void {
+    if (this.currentIndex < this.carouselImages.length - 1) {
+      this.currentIndex++;
+    }
+  }
 
   navigateToSignup() {
     this.router.navigate(['/signup']);
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
   }
 }

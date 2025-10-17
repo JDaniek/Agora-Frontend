49d9531d53e55b21f631424bc29c8da2b0b7036e
JDaniek <danielorantes270@gmail.com>
2025-10-17 09:28:50 -0600

    Menu desplegable agregado-dashboard de maestro

--- src/app/pages/dashboard-asesor/dashboard-asesor.ts
+++ src/app/pages/dashboard-asesor/dashboard-asesor.ts
@@ -7,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard-asesor.css'
 })
 export class DashboardAsesor {
+  isSidebarOpen = false;
 
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  closeSidebar() {
+    this.isSidebarOpen = false;
+  }
+    solicitudes = [
+    {
+      id: 1,
+      nombre: 'Carlos Méndez',
+      fecha: '16 oct 2025',
+      estado: 'Pendiente',
+      prioridad: 'Alta'
+    },
+    {
+      id: 2,
+      nombre: 'Ana López',
+      fecha: '15 oct 2025',
+      estado: 'Aprobada',
+      prioridad: 'Media'
+    },
+    {
+      id: 3,
+      nombre: 'Luis Ramírez',
+      fecha: '14 oct 2025',
+      estado: 'Rechazada',
+      prioridad: 'Baja'
+    },]
 }

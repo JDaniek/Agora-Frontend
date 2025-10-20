db0703cb4542d16277a3405a3b31065e3b04b81d
JDaniek <danielorantes270@gmail.com>
2025-10-19 20:43:08 -0600

    Correcion en los estilos de la vista del formulario y correccion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,28 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/landing-page/landing.page').then(m => m.LandingPage) },
-  { path: 'signup', loadComponent: () => import('./pages/signup/signup').then(m => m.Signup) },
-    { path: 'login', loadComponent: () => import('./pages/login/login').then(m => m.Login) },
-    { path: 'register', loadComponent: () => import('./pages/register/register').then(m => m.Register) },
-    { path: 'dashboard-asesor', loadComponent:()=> import ('./pages/dashboard-asesor/dashboard-asesor').then(m=> m.DashboardAsesor)}
+  {
+    path: '',
+    loadComponent: () => import('./pages/landing-page/landing.page').then((m) => m.LandingPage),
+  },
+  { path: 'signup', loadComponent: () => import('./pages/signup/signup').then((m) => m.Signup) },
+  { path: 'login', loadComponent: () => import('./pages/login/login').then((m) => m.Login) },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register').then((m) => m.Register),
+  },
+  {
+    path: 'dashboard-asesor',
+    loadComponent: () =>
+      import('./pages/dashboard-asesor/dashboard-asesor').then((m) => m.DashboardAsesor),
+  },
+  {
+    path: 'student-home',
+    loadComponent: () => import('./pages/student-home/student-home').then((m) => m.StudentHome),
+  },
 
+    {
+    path: 'complete-profile',
+    loadComponent: () => import('./pages/complete-profile/complete-profile').then((m) => m. CompleteProfile),
+  },
 ];
